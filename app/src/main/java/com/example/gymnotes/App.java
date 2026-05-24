272dccd1c848843cfba5f249ba63408f2674ccf8
Mike <telyatnikmixail07@yandex.ru>
2026-05-24 18:58:09 +0300

    Apply saved theme on app start

--- app/src/main/java/com/example/gymnotes/App.java
+++ app/src/main/java/com/example/gymnotes/App.java
@@ -1,12 +1,15 @@
 package com.example.gymnotes;
 
 import android.app.Application;
+
 import com.yandex.mapkit.MapKitFactory;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ThemeManager.applySavedTheme(this);
 
         MapKitFactory.setApiKey("a9aa08a8-98ea-4050-8671-0bbc66115483");
         MapKitFactory.initialize(this);
