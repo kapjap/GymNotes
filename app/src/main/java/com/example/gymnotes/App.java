a3930b6169eeb2bfa79f253e30be849ae1ed1767
Mike <telyatnikmixail07@yandex.ru>
2026-05-24 19:06:54 +0300

    Apply selected theme to every activity

--- app/src/main/java/com/example/gymnotes/App.java
+++ app/src/main/java/com/example/gymnotes/App.java
@@ -1,6 +1,9 @@
 package com.example.gymnotes;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+import android.view.View;
 
 import com.yandex.mapkit.MapKitFactory;
 
@@ -10,8 +13,49 @@ public class App extends Application {
         super.onCreate();
 
         ThemeManager.applySavedTheme(this);
+        registerGlobalThemeCallback();
 
         MapKitFactory.setApiKey("a9aa08a8-98ea-4050-8671-0bbc66115483");
         MapKitFactory.initialize(this);
+    }
+
+    private void registerGlobalThemeCallback() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                applyThemeWhenLayoutIsReady(activity);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                applyThemeWhenLayoutIsReady(activity);
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                applyThemeWhenLayoutIsReady(activity);
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+            }
+        });
+    }
+
+    private void applyThemeWhenLayoutIsReady(Activity activity) {
+        View root = activity.getWindow().getDecorView();
+        root.post(() -> ThemeManager.applyThemeToActivity(activity));
     }
 }
