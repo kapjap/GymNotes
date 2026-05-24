9daba9c7132f0f5d0cb1e909c36d12b7f65dedbf
Mike <telyatnikmixail07@yandex.ru>
2026-05-24 19:26:29 +0300

    Fix BaseActivity theme method call

--- app/src/main/java/com/example/gymnotes/BaseActivity.java
+++ app/src/main/java/com/example/gymnotes/BaseActivity.java
@@ -1,10 +1,7 @@
 package com.example.gymnotes;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,23 +11,5 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         ThemeManager.applySavedTheme(this);
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void setContentView(@LayoutRes int layoutResID) {
-        super.setContentView(layoutResID);
-        ThemeManager.applyThemeToActivity(this);
-    }
-
-    @Override
-    public void setContentView(View view) {
-        super.setContentView(view);
-        ThemeManager.applyThemeToActivity(this);
-    }
-
-    @Override
-    public void setContentView(View view, ViewGroup.LayoutParams params) {
-        super.setContentView(view, params);
-        ThemeManager.applyThemeToActivity(this);
     }
 }
